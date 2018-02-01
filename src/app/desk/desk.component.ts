accec93386c057e864f1bf3a96d0557c73089b79
Alan Cole <alantuneit@gmail.com>
2018-01-31 17:46:11 -0700

    sticky note now moves when dragged

--- src/app/desk/desk.component.ts
+++ src/app/desk/desk.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeskComponent implements OnInit {
 
-  constructor() { }
+        constructor() { }
 
-  ngOnInit() {
-  }
+        ngOnInit() {
+        }
 
-}
+    }
--- src/app/sticky-note/sticky-note.component.ts
+++ src/app/sticky-note/sticky-note.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sticky-note',
-  templateUrl: './sticky-note.component.html',
-  styleUrls: ['./sticky-note.component.css']
+    selector: 'app-sticky-note',
+    templateUrl: './sticky-note.component.html',
+    styleUrls: ['./sticky-note.component.css']
 })
+
 export class StickyNoteComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    moveNote(event) {
+        let position1 = 0;
+        let position2 = 0;
+        let position3 = 0;
+        let position4 = 0;
+
+        console.log('mousedown!');
+        const note = event.target;
+        note.classList.add('moving');
+        console.log(event);
+
+        position3 = event.clientX;
+        position4 = event.clientY;
+        console.log(position3, position4);
+
+        document.onmouseup = function() {
+
+          console.log('mouseup!, Did it move?');
+          note.classList.remove('moving');
+          document.onmouseup = null;
+          document.onmousemove = null;
+        };
+
+        document.onmousemove = function(ev) {
+          console.log(ev);
+          position1 = position3 - ev.clientX;
+          position2 = position4 - ev.clientY;
+          position3 = ev.clientX;
+          position4 = ev.clientY;
+
+          note.style.top = (note.offsetTop - position2) + 'px';
+          note.style.left = (note.offsetLeft - position1) + 'px';
+        };
+    }
 }
